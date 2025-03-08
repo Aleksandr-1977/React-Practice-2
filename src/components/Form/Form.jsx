e05e79319555ca7f19f7ba8918fc1f0f0b256238
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-03-08 13:07:02 +0200

    add fotos

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,25 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
 
-const Form = () => {
-  return <h2>Form</h2>;
+const Form = ({ onSubmit }) => {
+  const [fotos, setFotos] = useState('');
+  const handleChange = evt => {
+    setFotos(evt.target.value);
+  };
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    if (!fotos.trim()) {
+      return alert('Empty');
+    }
+    onSubmit(fotos);
+    setFotos('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="search" value={fotos} onChange={handleChange} />
+      <button type="submit">Search</button>
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from 'react';
 import Text from '../components/Text/Text';
+import Form from '../components/Form/Form';
+import Loader from '../components/Loader/Loader';
+import PhotosGallery from '../components/PhotosGallery/PhotosGallery';
+import { getPhotos } from '../apiService/photos';
 
 const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [image, setImage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (!query) return;
+    const fetchPhotos = async () => {
+      setIsLoading(true);
+      try {
+        const { photos, per_page, total_result } = await getPhotos(query, page);
+        if (!photos.length) {
+          setIsEmpty(true);
+          return;
+        }
+        setImage(prevImage => [...prevImage, ...photos]);
+        setIsVisible(page < Math.cell(total_result / per_page));
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPhotos();
+  }, [page, query]);
+
+  const getQuery = inputValue => {
+    setQuery(inputValue);
+  };
   return (
     <>
+      <Form onSubmit={getQuery} />
+      {isLoading && <Loader />}
+      {error && <Text>Ooops</Text>}
+      {!error && !isEmpty && <Text>Lets begin</Text>}
+      {isEmpty && <Text>Sorry</Text>}
+      {image.length > 0 && <PhotosGallery image={image} />}
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
